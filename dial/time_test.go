640e4bb765b27847fa486c8d21a9d823943fa609
Frederic G. MARAND <fgm@osinet.fr>
2018-11-12 21:28:11 +0100

    run gofmt -s on tests too.

--- dial/time_test.go
+++ dial/time_test.go
@@ -7,22 +7,22 @@ import (
 
 func TestIsTimeoutValid(t *testing.T) {
 	t.Parallel()
-	limit := time.Duration(float64(MaxDuration) / BackoffFactor).Truncate(1*time.Second)
+	limit := time.Duration(float64(MaxDuration) / BackoffFactor).Truncate(1 * time.Second)
 
-	attempts := []struct{
+	attempts := []struct {
 		duration time.Duration
 		expected bool
-		message string
+		message  string
 	}{
-		{ 0, false, "Zero duration is invalid" },
-		{ -1 * time.Second, false, "Negative duration cannot be waited for" },
-		{ MaxDuration, false, "Maximum duration cannot be extended"},
-		{ 1, true, "Minimum duration is valid"},
-		{ limit, true, "Limit duration is valid"},
+		{0, false, "Zero duration is invalid"},
+		{-1 * time.Second, false, "Negative duration cannot be waited for"},
+		{MaxDuration, false, "Maximum duration cannot be extended"},
+		{1, true, "Minimum duration is valid"},
+		{limit, true, "Limit duration is valid"},
 	}
 
 	for _, attempt := range attempts {
-		t.Run(attempt.message, func (t *testing.T) {
+		t.Run(attempt.message, func(t *testing.T) {
 			// t.Parallel()
 			actual := IsMaxTimeoutValid(attempt.duration)
 			if actual != attempt.expected {
